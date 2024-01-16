65a1302e9939b77540f2954dea3023aa68f3fcd7
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-16 14:05:34 -0300

    Incorporación de punto y coma en las importaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header"
-import Home from "./components/Home"
-import ContainDetails from "./components/ContainDetails"
-import ContainReleases from "./components/ContainReleases"
-import ContainPopular from "./components/ContainPopular"
-import ContainFavorites from "./components/ContainFavorites"
-import ContaineSearch from "./components/ContainSearch"
+import Header from "./components/Header";
+import Home from "./components/Home";
+import ContainDetails from "./components/ContainDetails";
+import ContainReleases from "./components/ContainReleases";
+import ContainPopular from "./components/ContainPopular";
+import ContainFavorites from "./components/ContainFavorites";
+import ContaineSearch from "./components/ContainSearch";
 
 function App() {
   return (
