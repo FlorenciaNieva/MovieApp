f70817064dab6fd4a21505d10658b96e9e3b1eb3
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-16 18:48:15 -0300

    Importo axios y creo función getPopular

--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
+import axios from "axios";
 
 const useMovies = () => {
   const [info, setInfo] = useState([]);
   const apiKey = import.meta.env.VITE_MOVIE_API_KEY;
 
+  const getPopular = () => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+      .then((response) => {
+        // console.log(response.data);
+        setInfo(response.data.results);
+      })
+      .catch((error) => console.error(error));
+  };
+
   return {
     info,
-  }
-}
+    getPopular
+  };
+};
 
-export default useMovies
+export default useMovies;
