41325c48c1cf72a26f7a25b97c1c3a50185a1d74
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-17 15:10:39 -0300

    Modifico las flechas del carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,6 +4,7 @@ import "../../node_modules/slick-carousel/slick/slick.css";
 import "../../node_modules/slick-carousel/slick/slick-theme.css";
 import useMovies from "../hooks/useMovies";
 import { Image, Box } from "@chakra-ui/react";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 export default function Carousel() {
   const { info, getNewMovies } = useMovies();
@@ -11,6 +12,42 @@ export default function Carousel() {
   useEffect(() => {
     getNewMovies();
   }, []);
+
+  const CustomPrevArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <Box
+        onClick={onClick}
+        position="absolute"
+        top="50%"
+        left="10px"
+        zIndex="1"
+        fontSize="30px"
+        cursor="pointer"
+        transform="translateY(-50%)"
+      >
+        <FaChevronLeft />
+      </Box>
+    );
+  };
+
+  const CustomNextArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <Box
+        onClick={onClick}
+        position="absolute"
+        top="50%"
+        right="10px"
+        zIndex="1"
+        fontSize="30px"
+        cursor="pointer"
+        transform="translateY(-50%)"
+      >
+        <FaChevronRight />
+      </Box>
+    );
+  };
 
   const settings = {
     dots: true,
@@ -20,6 +57,8 @@ export default function Carousel() {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 8000,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
   };
 
   return (
