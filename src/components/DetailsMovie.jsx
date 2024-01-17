9a3d1f4bdca99b8ec9288e3147e230dc281bcd11
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-17 16:23:28 -0300

    Importo y destructuro el hook useMovies

--- src/components/DetailsMovie.jsx
+++ src/components/DetailsMovie.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import useMovies from '../hooks/useMovies'
 
 export default function DetailsMovie() {
+  const {info, getMovieId} = useMovies();
+  
   return (
     <div>DetailsMovie</div>
   )
