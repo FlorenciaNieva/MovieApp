579ac4a89d4cbcd9f057416bf8f08e24ad134367
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-17 16:29:53 -0300

    Agrego useEffect para la actualización

--- src/components/DetailsMovie.jsx
+++ src/components/DetailsMovie.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useMovies from '../hooks/useMovies'
 import { useParams } from 'react-router';
 
 export default function DetailsMovie() {
   const {info, getMovieId} = useMovies();
   const params = useParams();
+  
+  useEffect(() => {
+    getMovieId(params.id);
+  }, [info]);
 
   return (
     <div>DetailsMovie</div>
