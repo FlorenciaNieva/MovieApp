d41cdbcce9273c0207752565655fded824e4938d
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-17 16:25:21 -0300

    Importo y guardo en una variable a el hook useParams

--- src/components/DetailsMovie.jsx
+++ src/components/DetailsMovie.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import useMovies from '../hooks/useMovies'
+import { useParams } from 'react-router';
 
 export default function DetailsMovie() {
   const {info, getMovieId} = useMovies();
-  
+  const params = useParams();
+
   return (
     <div>DetailsMovie</div>
   )
