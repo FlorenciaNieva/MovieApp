fea9fb2060c6d5a8e0995ada39bd21796b3ab8ca
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-17 20:08:56 -0300

    Agrego detalles de la película

--- src/components/DetailsMovie.jsx
+++ src/components/DetailsMovie.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import useMovies from "../hooks/useMovies";
 import { useParams } from "react-router";
-import { Box, Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { FaStar } from "react-icons/fa";
 
 export default function DetailsMovie() {
   const { info, getMovieId } = useMovies();
@@ -31,6 +32,22 @@ export default function DetailsMovie() {
             w={250}
             borderRadius={5}
           />
+          <Box ml={5}>
+            <Flex>
+              <Text as='b'>{info.vote_average?.toFixed(1)}</Text>
+              <Text ml={1} mt={1}>
+                <FaStar/>
+              </Text>
+            </Flex>
+            <Heading mb={3}>{info.title}</Heading>
+            {info.genres?.map((gen) => (
+              <Text as='b' mr={3} color="RGB(217 218 222)">{gen.name}</Text>
+            ))}
+            <Text>{info.runtime}</Text>
+            <Text>{new Date(info.release_date).getFullYear()}</Text>
+            <Heading as='h5' size='sm' mt="1">Overview</Heading>
+            <Text>{info.overview}</Text>
+          </Box>
         </Flex>
       </Box>
     </>
