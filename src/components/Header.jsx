770e837c6f4c6d99deb53524f96fb3787f6b408a
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-17 17:10:27 -0300

    Agrego etiquetas nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,24 +22,49 @@ export default function Header() {
           MovieApp
         </Heading>
         <Box flex="1" m="4" color="white">
-          <ChakraLink as={Link} to="/" m="2" _hover={{ color: "#ff2323" }}>
-            Home
-          </ChakraLink>
-          <ChakraLink as={Link} to="/releases" m="2" _hover={{ color: "#ff2323" }}>
-            Realeases
-          </ChakraLink>
-          <ChakraLink as={Link} to="/popular" m="2" _hover={{ color: "#ff2323" }}>
-            Popular
-          </ChakraLink>
+          <nav>
+            <ChakraLink as={Link} to="/" m="2" _hover={{ color: "#ff2323" }}>
+              Home
+            </ChakraLink>
+            <ChakraLink
+              as={Link}
+              to="/releases"
+              m="2"
+              _hover={{ color: "#ff2323" }}
+            >
+              Realeases
+            </ChakraLink>
+            <ChakraLink
+              as={Link}
+              to="/popular"
+              m="2"
+              _hover={{ color: "#ff2323" }}
+            >
+              Popular
+            </ChakraLink>
+          </nav>
         </Box>
         <Spacer />
         <Flex mt="4" mr="5" color="white" fontSize="25px">
-          <ChakraLink as={Link} to="/favorites" mr="5" _hover={{ color: "#ff2323" }} aria-label="Favorites">
-            <MdFavorite />
-          </ChakraLink>
-          <ChakraLink as={Link} to="/search" _hover={{ color: "#ff2323" }} aria-label="Search">
-            <FiSearch />
-          </ChakraLink>
+          <nav>
+            <ChakraLink
+              as={Link}
+              to="/favorites"
+              mr="5"
+              _hover={{ color: "#ff2323" }}
+              aria-label="Favorites"
+            >
+              <MdFavorite />
+            </ChakraLink>
+            <ChakraLink
+              as={Link}
+              to="/search"
+              _hover={{ color: "#ff2323" }}
+              aria-label="Search"
+            >
+              <FiSearch />
+            </ChakraLink>
+          </nav>
         </Flex>
       </Flex>
     </header>
