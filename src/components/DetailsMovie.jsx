82d9574a46541fb88f10a1dacec627ebc08e4c5b
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-17 20:06:04 -0300

    Agrego imagen de fondo

--- src/components/DetailsMovie.jsx
+++ src/components/DetailsMovie.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from 'react'
-import useMovies from '../hooks/useMovies'
-import { useParams } from 'react-router';
+import React, { useEffect } from "react";
+import useMovies from "../hooks/useMovies";
+import { useParams } from "react-router";
+import { Box, Image } from "@chakra-ui/react";
 
 export default function DetailsMovie() {
-  const {info, getMovieId} = useMovies();
+  const { info, getMovieId } = useMovies();
   const params = useParams();
-  
+
   useEffect(() => {
     getMovieId(params.id);
-  }, [info]);
+  }, [info.id]);
 
   return (
-    <div>DetailsMovie</div>
-  )
+    <>
+      <Box position="absolute" top={0}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original${info.backdrop_path}`}
+          alt={info.title}
+          w="100vw"
+          h="100vh"
+          filter="auto"
+          brightness="40%"
+        />
+      </Box>
+    </>
+  );
 }
