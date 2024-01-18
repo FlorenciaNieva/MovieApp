6b81eea4d1ea62583927d16e4357c47d5bc52fb2
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-18 20:45:53 -0300

    Agrego Youtube de react-youtube y le paso la prop trailerId en videoId

--- src/components/ModalTrailer.jsx
+++ src/components/ModalTrailer.jsx
@@ -5,6 +5,7 @@ import {
   ModalBody,
   ModalCloseButton,
 } from '@chakra-ui/react';
+import YouTube from 'react-youtube';
 
 export default function ModalTrailer({ isOpen, onClose, trailerId }) {
   return (
@@ -13,7 +14,26 @@ export default function ModalTrailer({ isOpen, onClose, trailerId }) {
         <ModalOverlay />
         <ModalContent>
           <ModalCloseButton color="white" />
-          <ModalBody bg="black" ></ModalBody>
+          <ModalBody bg="black" >
+            <YouTube
+              style={{ height: "80vh"}}
+              videoId={trailerId}
+              opts={{
+                width: "100%",
+                height: "100%",
+                playerVars: {
+                  autoplay: 1,
+                  controls: 0,
+                  cc_load_policy: 0,
+                  fs: 0,
+                  iv_load_policy: 0,
+                  modestbranding: 0,
+                  rel: 0,
+                  showinfo: 0,
+                },
+              }}
+            />
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
