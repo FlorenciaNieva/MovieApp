1bf6badb039002d5753ed91fd8e587479e31c2a8
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-19 15:57:21 -0300

    Creo la tarjeta para las peliculas

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { Card, Image, Box, Text } from "@chakra-ui/react";
 
 export default function CardMovie() {
   return (
-    <div>CardMovie</div>
-  )
+    <>
+      <Box m={5} maxW="160px">
+        <Card
+          maxW="100%"
+          h="250px"
+          borderRadius="10px"
+          mb={2}
+          position="relative"
+        >
+          <Image
+            src=""
+            alt=""
+            w="100%"
+            h="100%"
+            borderRadius="10px"
+          />
+        </Card>
+        <Text 
+          textAlign="center"
+          as="b"
+          overflow="hidden"
+          display="-webkit-box"
+          style={{
+            WebkitLineClamp: 1,
+            WebkitBoxOrient: 'vertical',
+            textOverflow: 'ellipsis',
+          }}
+        >
+          Name
+        </Text>
+      </Box>
+    </>
+  );
 }
