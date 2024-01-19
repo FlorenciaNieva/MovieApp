44a86a2b56bbd01200f7a8c02f6c481e1157460f
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-19 17:18:18 -0300

    Agrego carousel de react-multi-carousel

--- src/components/CarouselCards.jsx
+++ src/components/CarouselCards.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import CardMovie from "./CardMovie";
 
 export default function CarouselCards() {
   return (
-    <div>CarouselCards</div>
+    <>
+      <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlaySpeed={3000}
+        centerMode={true}
+        containerClass="container"
+        draggable
+        focusOnSelect={false}
+        infinite={false}
+        keyBoardControl
+        minimumTouchDrag={80}
+        pauseOnHover
+        renderArrowsWhenDisabled={false}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={{
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1024,
+            },
+            items: 5,
+            partialVisibilityGutter: 40,
+          },
+          mobile: {
+            breakpoint: {
+              max: 464,
+              min: 0,
+            },
+            items: 1,
+            partialVisibilityGutter: 30,
+          },
+          tablet: {
+            breakpoint: {
+              max: 1024,
+              min: 464,
+            },
+            items: 4,
+            partialVisibilityGutter: 30,
+          },
+        }}
+        rewind={false}
+        rewindWithAnimation={false}
+        rtl={false}
+        shouldResetAutoplay
+        showDots={false}
+        slidesToSlide={1}
+        swipeable
+      >
+        <CardMovie />
+      </Carousel>
+    </>
   );
 }
