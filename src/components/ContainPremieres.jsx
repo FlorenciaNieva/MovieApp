791be43bc010d0a4a5c0679b7692fde06e7e20bf
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-22 16:40:37 -0300

    Agrego titulo y renderizo las tarjetas de las peliculas en el contenedor

--- src/components/ContainPremieres.jsx
+++ src/components/ContainPremieres.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import useMovies from "../hooks/useMovies";
+import CardMovie from "./CardMovie";
+import { Flex, Heading } from "@chakra-ui/react";
 
 export default function ContainPremieres() {
   const { info, page, getNewMovies } = useMovies();
@@ -9,6 +11,17 @@ export default function ContainPremieres() {
   }, [page]);
 
   return (
-    <div>Contain Premieres</div>
-  )
+    <>
+      <Heading textAlign="center">Premieres Movies</Heading>
+      <Flex flexWrap="wrap" justifyContent="center">
+        {info.map((movie) => (
+          <CardMovie
+            id={movie.id}
+            name={movie.title}
+            poster={movie.poster_path}
+          />
+        ))}
+      </Flex>
+    </>
+  );
 }
