4b2e216b60f7459124de4967cd2a4ff32ff0c2c3
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-22 16:39:07 -0300

    Agrego destructuración de useMovies y useEffect con getNewMovies

--- src/components/ContainPremieres.jsx
+++ src/components/ContainPremieres.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { useEffect } from "react";
+import useMovies from "../hooks/useMovies";
 
 export default function ContainPremieres() {
+  const { info, page, getNewMovies } = useMovies();
+
+  useEffect(() => {
+    getNewMovies(page);
+  }, [page]);
+
   return (
     <div>Contain Premieres</div>
   )
