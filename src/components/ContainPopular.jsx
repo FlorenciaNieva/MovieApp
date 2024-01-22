4ba9f09a0d91ae86025930995d56449c75a85322
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-22 12:05:49 -0300

    import cardMovie y la renderizo al hacer el map de info

--- src/components/ContainPopular.jsx
+++ src/components/ContainPopular.jsx
@@ -1,14 +1,27 @@
-import { useEffect } from 'react'
-import useMovies from '../hooks/useMovies'
+import { useEffect } from "react";
+import useMovies from "../hooks/useMovies";
+import CardMovie from "./CardMovie";
+import { Flex, Heading } from "@chakra-ui/react";
 
 export default function ContainPopular() {
-  const {info, getPopular} = useMovies();
+  const { info, getPopular } = useMovies();
 
   useEffect(() => {
     getPopular();
   }, []);
 
   return (
-    <div>ContainPopular</div>
-  )
+    <>
+      <Heading textAlign="center">Popular Movies</Heading>
+      <Flex flexWrap="wrap" justifyContent="center">
+        {info.map((movie) => (
+          <CardMovie
+            id={movie.id}
+            name={movie.title}
+            poster={movie.poster_path}
+          />
+        ))}
+      </Flex>
+    </>
+  );
 }
