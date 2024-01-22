accec6d656b1f8d296eb6dde5c3f155b0905f308
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-22 12:02:10 -0300

    Traigo info y función getPopular de useMovie y la llamo en useEffect

--- src/components/ContainPopular.jsx
+++ src/components/ContainPopular.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { useEffect } from 'react'
+import useMovies from '../hooks/useMovies'
 
 export default function ContainPopular() {
+  const {info, getPopular} = useMovies();
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
   return (
     <div>ContainPopular</div>
   )
