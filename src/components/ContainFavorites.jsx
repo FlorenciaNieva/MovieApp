a57907aa8582c42a96a193c607c5ec0e233d75a7
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-24 17:42:54 -0300

    Agrego tarjetas de las peliculas favoritas

--- src/components/ContainFavorites.jsx
+++ src/components/ContainFavorites.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useContext } from 'react'
+import { FavoritesContext } from '../context/favoritesContext';
+import { Flex, Heading } from '@chakra-ui/react';
+import CardMovie from './CardMovie';
 
 export default function ContainFavorites() {
+  const { allFavorites } = useContext(FavoritesContext);
+
   return (
-    <div>ContainFavorites</div>
+    <>
+      <Heading textAlign="center">Favorites</Heading>
+      <Flex flexWrap="wrap" justifyContent="center">
+        {allFavorites.map((movie) => (
+          <CardMovie
+            id={movie.id}
+            name={movie.name}
+            poster={movie.image}
+          />
+        ))}
+      </Flex>
+    </>
   )
 }
