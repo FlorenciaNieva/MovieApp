f4f8a0d9bc6eab72144b31eb569f6b8a688a0435
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-24 16:31:12 -0300

    Agrego estado allFavorites

--- src/context/favoritesContext.jsx
+++ src/context/favoritesContext.jsx
@@ -1,8 +1,10 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const FavoritesContext = createContext();
 
 const FavoritesContextProvider = ({ children }) => {
+  const [allFavorites, setAllFavorites] = useState([]);
+
   const data = {};
 
   return (
