270315bb4631fc653866b219dd2e3e84738ed323
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-26 13:59:28 -0300

    Agrego el componente Footer y modifico alturas de los contenedores

--- src/components/ContainFavorites.jsx
+++ src/components/ContainFavorites.jsx
@@ -3,6 +3,7 @@ import { FavoritesContext } from "../context/favoritesContext";
 import { Flex, Heading } from "@chakra-ui/react";
 import CardMovie from "./CardMovie";
 import { RiHeartAddFill } from "react-icons/ri";
+import Footer from "./Footer";
 
 export default function ContainFavorites() {
   const { allFavorites } = useContext(FavoritesContext);
@@ -11,17 +12,18 @@ export default function ContainFavorites() {
     <>
       <Heading textAlign="center">Favorites</Heading>
       {allFavorites.length === 0 ? (
-        <Flex h="80vh" justifyContent="center" alignItems="center">
+        <Flex h="90vh" justifyContent="center" alignItems="center">
           <Heading color="#93959C" mr={2}>Add Favorites</Heading>
           <RiHeartAddFill color="#93959C" fontSize="3rem" />
         </Flex>
       ) : (
-        <Flex flexWrap="wrap" justifyContent="center">
+        <Flex flexWrap="wrap" justifyContent="center" minH="85vh">
           {allFavorites.map((movie) => (
             <CardMovie id={movie.id} name={movie.name} poster={movie.image} />
           ))}
         </Flex>
       )}
+      <Footer position="absolute" bottom="-12vh" />
     </>
   )
 }
