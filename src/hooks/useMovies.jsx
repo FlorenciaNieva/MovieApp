685b29ec56f83f145cefa0fa63fe7ead74c5fc74
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-26 15:23:19 -0300

    Agrego try, catch, async y await a las funciones en useMovies

--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -9,66 +9,72 @@ const useMovies = () => {
   const [trailer, setTrailer] = useState(null);
   const apiKey = import.meta.env.VITE_MOVIE_API_KEY;
 
-  const getPopular = (page) => {
-    axios(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${page}`)
-      .then((response) => {
-        setInfo(response.data.results);
-        setTotalPage(response.data.total_results);
-        setCurrentPage(response.data.page);
-      })
-      .catch((error) => console.error(error));
+  const getPopular = async (page) => {
+    try {
+      const response = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${page}`);
+      setInfo(response.data.results);
+      setTotalPage(response.data.total_results);
+      setCurrentPage(response.data.page);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const getNewMovies = (page) => {
-    axios(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${page}`)
-      .then((response) => {
-        setInfo(response.data.results);
+  const getNewMovies = async (page) => {
+    try {
+      const response = await axios(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${page}`);
+      setInfo(response.data.results);
         setTotalPage(response.data.total_results);
         setCurrentPage(response.data.page);
-      })
-      .catch((error) => console.error(error));
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handlePageChange = (newPage) =>{
     setPage(newPage);
   };
 
-  const getTopRated = () => {
-    axios(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
-      .then((response) => {
-        setInfo(response.data.results);
-      })
-      .catch((error) => console.error(error));
+  const getTopRated = async () => {
+    try {
+      const response = await axios(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`);
+      setInfo(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const getMovieId = (id) => {
-    axios(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
-      .then((response) => {
-        setInfo(response.data);
-      })
-      .catch((error) => console.error(error));
-  }
-
-  const getVideo = (id) => {
-    axios(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&append_to_response=videos`)
-      .then((response) => {
-        const videos = response.data.videos.results;
-        const videoOfficial = videos.find(
-          (video) => video.name === "Official Trailer"
-        );
-        setTrailer(
-          videoOfficial ? videoOfficial : response.data.videos.results[0]
-        );
-      })
-      .catch((error) => console.error(error));
+  const getMovieId = async (id) => {
+    try {
+      const response = await axios(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`);
+      setInfo(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const searchMovie = (movie) => {
-    axios(`https://api.themoviedb.org/3/search/movie?query=${movie}&api_key=${apiKey}`)
-      .then((response) => {
-        setInfo(response.data.results);
-      })
-      .catch((error) => console.error(error));
+  const getVideo = async (id) => {
+    try {
+      const response = await axios(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&append_to_response=videos`);
+      const videos = response.data.videos.results;
+      const videoOfficial = videos.find(
+        (video) => video.name === "Official Trailer"
+      );
+      setTrailer(
+        videoOfficial ? videoOfficial : response.data.videos.results[0]
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const searchMovie = async (movie) => {
+    try {
+      const response = await axios(`https://api.themoviedb.org/3/search/movie?query=${movie}&api_key=${apiKey}`);
+      setInfo(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return {
