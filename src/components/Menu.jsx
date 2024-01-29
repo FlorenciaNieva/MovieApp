ab768781864ccc53914dd4cd092eb1321565bba7
FlorenciaNieva <florencianieva930@gmail.com>
2024-01-29 18:00:44 -0300

    Agrego menu con los links de navegación

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import {
+  Menu as MenuChakra,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Link as ChakraLink,
+  IconButton,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { IoMenu } from "react-icons/io5";
 
 export default function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <MenuChakra>
+      <MenuButton
+        as={IconButton}
+        aria-label="Navegation"
+        variant="outline"
+        border="none"
+        icon={<IoMenu />}
+        bg="transparent"
+        color="red"
+        fontSize="25px"
+        _hover="none"
+        _focus="none"
+        _active="none"
+      />
+      <MenuList color="white" bg="black">
+        <MenuItem as={Link} to="/" bg="black" _hover={{ color: "#ff2323" }}>
+          Home
+        </MenuItem>
+        <MenuItem
+          as={Link}
+          to="/premieres"
+          bg="black"
+          _hover={{ color: "#ff2323" }}
+        >
+          Premieres
+        </MenuItem>
+        <MenuItem
+          as={Link}
+          to="/popular"
+          bg="black"
+          _hover={{ color: "#ff2323" }}
+        >
+          Popular
+        </MenuItem>
+        <MenuItem
+          as={Link}
+          to="/favorites"
+          bg="black"
+          _hover={{ color: "#ff2323" }}
+        >
+          Favorites
+        </MenuItem>
+        <MenuItem
+          as={Link}
+          to="/search"
+          bg="black"
+          _hover={{ color: "#ff2323" }}
+        >
+          Search
+        </MenuItem>
+      </MenuList>
+    </MenuChakra>
+  );
 }
