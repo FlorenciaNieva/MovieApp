963beb6df1a43678f628855d1516e8f09a21c0b9
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-01 16:35:06 -0300

    Agrego el condicional a una función y dejo un único return para renderizar el carusel de las tarjetas

--- src/components/ContainCarouselCards.jsx
+++ src/components/ContainCarouselCards.jsx
@@ -1,27 +1,23 @@
-import React, { useEffect } from 'react';
-import CarouselCards from './CarouselCards';
-import useMovies from '../hooks/useMovies';
+import React, { useEffect } from "react";
+import CarouselCards from "./CarouselCards";
+import useMovies from "../hooks/useMovies";
 
 export default function ContainCarouselCards({ category }) {
-  const {info, page, getPopular, getTopRated} = useMovies();
+  const { info, page, getPopular, getTopRated } = useMovies();
 
-  if(category === "popular" ) {
-    useEffect(() => {
-      getPopular(page);
-    }, []);
-    return (
-      <>
-        <CarouselCards info={info}/>
-      </>
-    )
-  } else {
-    useEffect(() => {
-      getTopRated();
-    }, []);
-    return (
-      <>
-        <CarouselCards info={info} />
-      </>
-    )
-  }
+  const data = () => {
+    if (category === "popular") {
+      useEffect(() => {
+        getPopular(page);
+      }, []);
+      return info;
+    } else {
+      useEffect(() => {
+        getTopRated();
+      }, []);
+      return info;
+    }
+  };
+
+  return <CarouselCards info={data()} />;
 }
