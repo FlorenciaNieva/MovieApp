d975223b28b862b69d7baa0c6245323d4d0e48b9
FlorenciaNieva <florencianieva930@gmail.com>
2024-02-01 16:45:52 -0300

    Modifico el formato del código

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
-import { useEffect } from 'react';
-import CarouselBanner from './CarouselBanner';
-import { Box, Heading } from '@chakra-ui/react';
-import ContainCarouselCards from './ContainCarouselCards';
-import Footer from './Footer';
-import useMovies from '../hooks/useMovies';
+import { useEffect } from "react";
+import CarouselBanner from "./CarouselBanner";
+import { Box, Heading } from "@chakra-ui/react";
+import ContainCarouselCards from "./ContainCarouselCards";
+import Footer from "./Footer";
+import useMovies from "../hooks/useMovies";
 
 export default function Home() {
   const { info, page, getNewMovies } = useMovies();
@@ -16,10 +16,14 @@ export default function Home() {
     <>
       <CarouselBanner info={info} />
       <Box position="relative" top="90vh">
-        <Heading as='h3' size='lg' pl={5}>Popular Movies</Heading> 
-        <ContainCarouselCards category="popular"/>
-        <Heading as='h3' size='lg' pl={5}>Top Ranking Movies</Heading>
-        <ContainCarouselCards category="top_rated"/>
+        <Heading as="h3" size="lg" pl={5}>
+          Popular Movies
+        </Heading>
+        <ContainCarouselCards category="popular" />
+        <Heading as="h3" size="lg" pl={5}>
+          Top Ranking Movies
+        </Heading>
+        <ContainCarouselCards category="top_rated" />
       </Box>
       <Footer position="relative" bottom="-90vh" />
     </>
