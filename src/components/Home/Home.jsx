b58f34c652d8e9eb6052962a98e8efc1755df90f
FlorenciaNieva <florencianieva930@gmail.com>
2024-03-08 13:43:18 -0300

    Elimino useEffect traia información y se lo pasaba al CarouselBanner

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,18 +3,11 @@ import CarouselBanner from "../CarouselBanner/CarouselBanner";
 import { Box, Heading } from "@chakra-ui/react";
 import ContainCarouselCards from "../ContainCarouselCards/ContainCarouselCards";
 import Footer from "../Footer/Footer";
-import useMovies from "../../hooks/useMovies";
 
 export default function Home() {
-  const { info, page, getNewMovies } = useMovies();
-
-  useEffect(() => {
-    getNewMovies(page);
-  }, []);
-
   return (
     <>
-      <CarouselBanner info={info} />
+      <CarouselBanner />
       <Box position="relative" top="90vh">
         <Heading as="h3" size="lg" pl={5}>
           Popular Movies
