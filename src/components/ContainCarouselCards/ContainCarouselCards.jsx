a0df6f44ae367eb188e4e81ef8b56beaac681e5d
FlorenciaNieva <florencianieva930@gmail.com>
2024-03-08 13:56:20 -0300

    Agrego el titulo desde el carousel de trajetas para terminar de hacer reutiliable al componente

--- src/components/ContainCarouselCards/ContainCarouselCards.jsx
+++ src/components/ContainCarouselCards/ContainCarouselCards.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
+import { Heading } from "@chakra-ui/react";
 import CarouselCards from "../CarouselCards/CarouselCards";
 import useMovies from "../../hooks/useMovies";
 
-export default function ContainCarouselCards({ category }) {
+export default function ContainCarouselCards({ title, category }) {
   const { info, page, getPopular, getTopRated } = useMovies();
 
   const data = () => {
@@ -19,5 +20,12 @@ export default function ContainCarouselCards({ category }) {
     }
   };
 
-  return <CarouselCards info={data()} />;
+  return (
+    <>
+      <Heading as="h3" size="lg" pl={5}>
+        {title}
+      </Heading>
+      <CarouselCards info={data()} />
+    </>
+  );
 }
