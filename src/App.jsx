13631c4ad2db063726ea1617f0db9899ad8de7aa
FlorenciaNieva <florencianieva930@gmail.com>
2024-03-08 13:35:23 -0300

    Eliminación de app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import Header from "./components/Header/Header";
