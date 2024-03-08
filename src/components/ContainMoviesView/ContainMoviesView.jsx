7152d3603265d49b1ea2d7b0526d8b475c4252b4
FlorenciaNieva <florencianieva930@gmail.com>
2024-03-08 14:40:52 -0300

    Agrego useEffect para traer la informacion de las peliculas y las renderizo junto al paginador y el footer

--- src/components/ContainMoviesView/ContainMoviesView.jsx
+++ src/components/ContainMoviesView/ContainMoviesView.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import { useEffect } from "react";
+import useMovies from "../../hooks/useMovies";
+import CardMovie from "../CardMovie/CardMovie";
+import { Flex, Heading } from "@chakra-ui/react";
+import BottomPagination from "../Pagination/BottomPagination";
+import Footer from "../Footer/Footer";
 
-export default function ContainMoviesView() {
+export default function ContainMoviesView({title, category}) {
+  const { 
+    info,
+    page, 
+    currentPage, 
+    handlePageChange,
+    totalPage, 
+    getMovies
+  } = useMovies();
+
+  useEffect(() => {
+    getMovies(page, category);
+  }, [page]);
+
   return (
-    <div>ContainMoviesView</div>
-  )
+    <>
+      <Heading textAlign="center">{title}</Heading>
+      <Flex flexWrap="wrap" justifyContent="center" minH="78vh">
+        {info.map((movie) => (
+          <CardMovie
+            key={movie.id}
+            id={movie.id}
+            name={movie.title}
+            poster={movie.poster_path}
+          />
+        ))}
+      </Flex>
+      <BottomPagination
+        currentPage={currentPage}
+        totalPage={totalPage}
+        handlePageChange={handlePageChange}
+        page={page}
+      />
+      <Footer position="relative" bottom="0" />
+    </>
+  );
 }
