fd9ba1db9dca91ee379ba0b327adbadbf072fed2
FlorenciaNieva <florencianieva930@gmail.com>
2024-03-08 14:14:32 -0300

    Agreog función data() y heading del componente ContainCarouselCards a CarouselCards

--- src/components/CarouselCards/CarouselCards.jsx
+++ src/components/CarouselCards/CarouselCards.jsx
@@ -1,74 +1,98 @@
+import { useEffect } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import { Heading } from "@chakra-ui/react";
 import CardMovie from "../CardMovie/CardMovie";
+import useMovies from "../../hooks/useMovies";
 
-export default function CarouselCards({ info }) {
+export default function CarouselCards({ title, category }) {
+  const { info, page, getPopular, getTopRated } = useMovies();
+
+  const data = () => {
+    if (category === "popular") {
+      useEffect(() => {
+        getPopular(page);
+      }, []);
+      return info;
+    } else {
+      useEffect(() => {
+        getTopRated();
+      }, []);
+      return info;
+    }
+  };
+
   return (
-    <Carousel
-      additionalTransfrom={0}
-      arrows
-      autoPlaySpeed={3000}
-      centerMode={false}
-      containerClass="container"
-      draggable
-      focusOnSelect={false}
-      infinite={false}
-      keyBoardControl
-      minimumTouchDrag={80}
-      pauseOnHover
-      renderArrowsWhenDisabled={false}
-      renderButtonGroupOutside={false}
-      renderDotsOutside={false}
-      responsive={{
-        desktop: {
-          breakpoint: {
-            max: 3000,
-            min: 1280,
+    <>
+      <Heading as="h3" size="lg" pl={5}>
+        {title}
+      </Heading>
+      <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlaySpeed={3000}
+        centerMode={false}
+        containerClass="container"
+        draggable
+        focusOnSelect={false}
+        infinite={false}
+        keyBoardControl
+        minimumTouchDrag={80}
+        pauseOnHover
+        renderArrowsWhenDisabled={false}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={{
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1280,
+            },
+            items: 8,
+            partialVisibilityGutter: 1,
           },
-          items: 8,
-          partialVisibilityGutter: 1,
-        },
-        laptop: {
-          breakpoint: {
-            max: 1280,
-            min: 720,
+          laptop: {
+            breakpoint: {
+              max: 1280,
+              min: 720,
+            },
+            items: 5,
+            partialVisibilityGutter: 30,
           },
-          items: 5,
-          partialVisibilityGutter: 30,
-        },
-        tablet: {
-          breakpoint: {
-            max: 720,
-            min: 464,
+          tablet: {
+            breakpoint: {
+              max: 720,
+              min: 464,
+            },
+            items: 4,
+            partialVisibilityGutter: 30,
           },
-          items: 4,
-          partialVisibilityGutter: 30,
-        },
-        mobile: {
-          breakpoint: {
-            max: 464,
-            min: 0,
+          mobile: {
+            breakpoint: {
+              max: 464,
+              min: 0,
+            },
+            items: 3,
+            partialVisibilityGutter: 30,
           },
-          items: 3,
-          partialVisibilityGutter: 30,
-        },
-      }}
-      rewind={false}
-      rewindWithAnimation={false}
-      rtl={false}
-      shouldResetAutoplay
-      showDots={false}
-      slidesToSlide={1}
-      swipeable
-    >
-      {info.map((movie) => (
-        <CardMovie
-          key={movie.id}
-          id={movie.id}
-          name={movie.title}
-          poster={movie.poster_path}
-        />
-      ))}
-    </Carousel>
+        }}
+        rewind={false}
+        rewindWithAnimation={false}
+        rtl={false}
+        shouldResetAutoplay
+        showDots={false}
+        slidesToSlide={1}
+        swipeable
+      >
+        {data().map((movie) => (
+          <CardMovie
+            key={movie.id}
+            id={movie.id}
+            name={movie.title}
+            poster={movie.poster_path}
+          />
+        ))}
+      </Carousel>
+    </>
   );
 }
